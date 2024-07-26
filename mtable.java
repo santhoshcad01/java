c831fe9ff5b4398771584c04b122f4629abc6158
santhoshcad01 <150434646+santhoshcad01@users.noreply.github.com>
2024-07-25 22:12:52 -0700

    for each method using java

--- mtable.java
+++ mtable.java
@@ -1,19 +1,49 @@
+// import java.util.*;
+
+// public class mtable {
+//     public static void main(String args[]){
+//         int mta,limit;
+//         Scanner num=new Scanner(System.in);
+//         System.out.println("Enter the number of multiple table=");
+//         mta=num.nextInt();
+//         System.out.println("Enter the limit of the table =");
+//         limit=num.nextInt();
+
+//         for(int i=1;i<=limit;i++)
+//         {
+//             System.out.println(i+"*"+mta+"="+i*mta);
+
+//         }
+    
+// }
+// }
+
+// import java.util.*;
+// class mtable{
+//     public static void main(String args[]){
+//         Scanner scan=new Scanner(System.in);
+//         int size=scan.nextInt();
+//         int[] num=new int[size];
+
+//         for(int i=0;i<=size;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
 import java.util.*;
 
-public class mtable {
+public class mtable{
     public static void main(String args[]){
-        int mta,limit;
-        Scanner num=new Scanner(System.in);
-        System.out.println("Enter the number of multiple table=");
-        mta=num.nextInt();
-        System.out.println("Enter the limit of the table =");
-        limit=num.nextInt();
-
-        for(int i=1;i<=limit;i++)
+        
+        for(int i=0;i<=3;i++)
         {
-            System.out.println(i+"*"+mta+"="+i*mta);
+
+            for(int j=1;j<=2;j++)
+            {
+            System.out.println("john");
+            }
 
         }
-    
-}
+    }
 }

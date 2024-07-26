2e019f00bc7d7dcb139b2a71275c661e8b7c5efb
santhoshcad01 <150434646+santhoshcad01@users.noreply.github.com>
2024-07-25 23:16:26 -0700

    Star problem using Nested Loop

--- mtable.java
+++ mtable.java
@@ -31,19 +31,42 @@
 //     }
 // }
 
-import java.util.*;
+//for each method:
 
+// import java.util.*;
+
+// public class mtable{
+//     public static void main(String args[]){
+        
+//         for(int i=0;i<=3;i++)
+//         {
+
+//             for(int j=1;j<=2;j++)
+//             {
+//             System.out.println("john");
+//             }
+
+//         }
+//     }
+// }
+
+
+// Nested loop to Start problem:
+
+import java.util.Scanner;
 public class mtable{
     public static void main(String args[]){
-        
-        for(int i=0;i<=3;i++)
-        {
-
-            for(int j=1;j<=2;j++)
+        int num;
+        Scanner star=new Scanner(System.in);
+        System.out.println("how many stars to show :");
+        num=star.nextInt();
+    
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=i;j++)
             {
-            System.out.println("john");
+                System.out.print("*");
             }
-
+             System.out.println();
         }
     }
 }

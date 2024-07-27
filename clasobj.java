1d9ce74bc575e4625bb20879075eac85c0a2289c
santhoshcad01 <150434646+santhoshcad01@users.noreply.github.com>
2024-07-27 10:25:00 -0700

    class and object method and function method using java

---
+++ clasobj.java
@@ -0,0 +1,68 @@
+/* public class clasobj{
+    String labname="";
+    String proc="";
+    int ram=0;
+    int price=0; 
+
+    public static void main(String args[]){
+        
+        clasobj lab1=new clasobj();
+
+        lab1.labname="lenova";
+        lab1.proc="i3";
+        lab1.ram=4;
+        lab1.price=30000;
+
+        clasobj lab2=new clasobj();
+
+        lab2.labname="LG";
+        lab2.proc="i5";
+        lab2.ram=6;
+        lab2.price=50000;
+
+        System.out.println(lab1.price);
+    }
+}
+    */
+     
+    //Function method:
+
+   /* public class clasobj{
+
+        void iphone()
+        {
+            System.out.println("This is a Iphone");
+        }
+        void oppo()
+        {
+            System.out.println("This is a OPPO Phone");
+        }
+
+        public static void main(String args[]){
+
+            clasobj phone=new clasobj();
+
+            phone.iphone();
+            
+        }
+    }
+
+    */
+
+
+    class clasobj{
+        int a=10;
+        int b=20;
+                
+        void sum(){
+           System.out.println(a+b);
+
+        }
+
+        public static void main(String args[]){
+
+            clasobj add=new clasobj();
+
+            add.sum();
+        }
+    }
